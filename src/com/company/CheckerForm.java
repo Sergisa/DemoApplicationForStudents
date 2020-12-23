118d8ef5a567f597c6803832217cf7e6b9ac0c10
sergisa <phebra@yandex.ru>
2020-12-23 15:58:01 +0300

    Final result

--- src/com/company/CheckerForm.java
+++ src/com/company/CheckerForm.java
@@ -1,9 +1,10 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CheckerForm extends JFrame {
     private JPanel panel1;
@@ -17,12 +18,26 @@ public class CheckerForm extends JFrame {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ImageIcon imageIcon = new ImageIcon(
+                // ^\d{4} \d{4} \d{4} (\d{4}|\d{6})$
+                // \w+@\w+\.(ru|com|net|ua|org)
+                // ([+]?[78]\([\d]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2})
+                // \d{1,2}[:-]\d{1,2}[:-]\d{1,2}
+                // (\w+)@\w+\.(ru|com|net|ua|org)
+                Pattern emailPattern = Pattern.compile("\\w+@\\w+\\.(ru|com|net|ua|org)");
+                Matcher emailPatternMatcher = emailPattern.matcher(textField1.getText());
+                if (emailPatternMatcher.find()) {
+
+                    /*ImageIcon imageIcon = new ImageIcon(
                         new ImageIcon("src/origUnresized.png")
                                 .getImage()
                                 .getScaledInstance(16, 16, Image.SCALE_DEFAULT)
-                );
-                responseLabel.setIcon(imageIcon);
+                    );*/
+                    responseLabel.setIcon(new ImageIcon("src/origUnresized.png"));
+                    System.out.println("good");
+                } else {
+                    responseLabel.setIcon(new ImageIcon("src/origUnresized.png"));
+                    System.out.println("bad");
+                }
             }
         });
 
