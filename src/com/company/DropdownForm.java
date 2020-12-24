c63c18f30fa474660939619d8cf04626aa138201
sergisa <phebra@yandex.ru>
2020-12-24 15:31:12 +0300

    Параметризированный вариант использования выпадающего списка Такой вариант самый адекватный.

--- src/com/company/DropdownForm.java
+++ src/com/company/DropdownForm.java
@@ -5,34 +5,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DropdownForm extends JFrame {
-    private JComboBox comboBox1;
+    private JComboBox<Cat> comboBox1;
     private JLabel visualizationLabel;
     private JPanel mainPanel;
 
     public DropdownForm() {
-        setSize(600, 200);
+        setSize(660, 600);
         setContentPane(mainPanel);
-
-        String[] catNames = new String[]{
-                "Аманта",
-                "Арабелла",
-                "Ахия"
-        };
 
         Cat[] cats = new Cat[]{
                 new Cat("Аманта"),
                 new Cat("Арабелла"),
                 new Cat("Ахия")
         };
-
-        for (String cat : catNames) {
+        for (Cat cat : cats) {
             comboBox1.addItem(cat);
         }
 
         comboBox1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                visualizationLabel.setText(cats[comboBox1.getSelectedIndex()].toString());
+                visualizationLabel.setText(((Cat) comboBox1.getSelectedItem()).getName());
             }
         });
     }
