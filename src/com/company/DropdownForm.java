d847997bdf9e89cdb0e26c1cd96eb6dd057a9627
sergisa <phebra@yandex.ru>
2020-12-24 14:22:09 +0300

    Ещё один вариант использования выпадающего списка Это СОВСЕМ неграмотный вариант использования возможностей библиотек.

--- src/com/company/DropdownForm.java
+++ src/com/company/DropdownForm.java
@@ -13,23 +13,31 @@ public class DropdownForm extends JFrame {
         setSize(600, 200);
         setContentPane(mainPanel);
 
-        String[] elementsToChoose = new String[]{
-                "Giraffe",
-                "Hedgehog",
-                "Cat",
-                "Dog",
-                "Elephant"
-        };
-        for (String element : elementsToChoose) {
-            comboBox1.addItem(element);
+
+        for (PaymentType type : PaymentType.values()) {
+            comboBox1.addItem(type.toString());
         }
 
         comboBox1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int selectedIndex = comboBox1.getSelectedIndex();
-                System.out.println(elementsToChoose[selectedIndex]);
+                if (selectedIndex == 0) {
+                    pay(PaymentType.CARD);
+                } else if (selectedIndex == 0) {
+                    pay(PaymentType.CARD);
+                }
+                //System.out.println(elementsToChoose[selectedIndex]);
             }
         });
+    }
+
+    enum PaymentType {
+        CASH,
+        CARD
+    }
+
+    public void pay(PaymentType selectedPaymentType) {
+
     }
 }
