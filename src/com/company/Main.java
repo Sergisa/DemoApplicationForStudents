6ec0760237f57a681e523404cc2100f5c4b32c51
Sergisa <phebra@yandex.ru>
2021-03-02 23:55:10 +0300

    FileReader + BufferedReader
    простой пример показывающий принцип действия функции readLine объекта BufferedReader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,20 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    static FileReader reader;
-
+    static BufferedReader bufferedReader;
     public static void main(String[] args) {
         try {
-            reader = new FileReader("src/com/company/data.txt");
-            while (reader.ready()){
-                char data = (char)reader.read();
-                System.out.print(data);
-            }
-            reader.close();
+            bufferedReader = new BufferedReader(new FileReader("src/com/company/data.txt"));
+            System.out.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
