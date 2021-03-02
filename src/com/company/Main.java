443ae06a2761954f93630bd283f1ee7cdc591d7d
Sergisa <phebra@yandex.ru>
2021-03-02 23:48:01 +0300

    Простое чтение файла
    Чтение файла символ за символом до конца.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
-import javax.swing.*;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+    static FileReader reader;
 
     public static void main(String[] args) {
-
+        try {
+            reader = new FileReader("src/com/company/data.txt");
+            while (reader.ready()){
+                char data = (char)reader.read();
+                System.out.print(data);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
