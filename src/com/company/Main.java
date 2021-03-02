9d66ab6a692d52d1fa8107c44651175d5ca967eb
Sergisa <phebra@yandex.ru>
2021-03-03 00:06:43 +0300

    FileReader + BufferedReader + File
    использование объекта File вместо строкового пути.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,8 +9,9 @@ public class Main {
     static BufferedReader bufferedReader;
     public static void main(String[] args) {
         String line;
+        File file = new File("src/com/company/data.txt");
         try {
-            bufferedReader = new BufferedReader(new FileReader("src/com/company/data.txt"));
+            bufferedReader = new BufferedReader(new FileReader(file));
             while((line = bufferedReader.readLine()) != null){
                 System.out.println(line);
             }
